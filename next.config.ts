9533d28b6badd6596be6b1d5644ee133ab06639d
Daniel Mohns <dmohns@users.noreply.github.com>
2024-11-13 12:55:53 +0100

    Support Github Pages with custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  assetPrefix:
-    process.env.NODE_ENV === "production" ? "/digitalization-support-hub" : "",
-  basePath:
-    process.env.NODE_ENV === "production" ? "/digitalization-support-hub" : "",
   output: "export",
   trailingSlash: true,
   images: {
