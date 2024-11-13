a4a481af9972af045ccc6539551bd6010a52e690
Munyaneza Armel <73389979+munyanezaarmel@users.noreply.github.com>
2024-11-13 13:30:48 +0200

    fix builds (#4)
    * fix builds
    
    * fix builds

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/digitalization-support-hub" : "",
+  basePath:
+    process.env.NODE_ENV === "production" ? "/digitalization-support-hub" : "",
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 }
 
 export default nextConfig
