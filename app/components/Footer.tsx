a2d4ef80aaccfa24fc0993f83be64a4b70b860a4
mark8-1 <munyaarmel61@gmail.com>
2025-01-28 19:09:47 +0200

    fix lint

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -26,8 +26,18 @@ export default function Footer() {
           <div>
             <p className="text-gray-600 mb-4">Made by:</p>
             <div className="flex items-center gap-8">
-              <Image src="/supporter1.png" width={120} height={40} alt="EnAccess logo" />
-              <Image src="/supporter1.png" width={120} height={40} alt="GET.invest logo" />
+              <Image
+                src="/supporter1.png"
+                width={120}
+                height={40}
+                alt="EnAccess logo"
+              />
+              <Image
+                src="/supporter1.png"
+                width={120}
+                height={40}
+                alt="GET.invest logo"
+              />
             </div>
           </div>
           <div className="flex flex-col items-start md:items-end">
@@ -44,17 +54,32 @@ export default function Footer() {
         <div className="mb-12">
           <p className="text-gray-600 mb-4">Other supporters:</p>
           <div className="flex justify-center gap-8">
-            <Image src="/supporter1.png" width={100} height={30} alt="Supporter 1 logo" />
-            <Image src="/supporter1.png" width={100} height={30} alt="Supporter 2 logo" />
-            <Image src="/supporter1.png" width={100} height={30} alt="Supporter 3 logo" />
+            <Image
+              src="/supporter1.png"
+              width={100}
+              height={30}
+              alt="Supporter 1 logo"
+            />
+            <Image
+              src="/supporter1.png"
+              width={100}
+              height={30}
+              alt="Supporter 2 logo"
+            />
+            <Image
+              src="/supporter1.png"
+              width={100}
+              height={30}
+              alt="Supporter 3 logo"
+            />
           </div>
         </div>
 
         <div className="text-center text-gray-600 text-sm">
-          © {new Date().getFullYear()} Digitalization Support Hub. All rights reserved.
+          © {new Date().getFullYear()} Digitalization Support Hub. All rights
+          reserved.
         </div>
       </div>
     </footer>
   )
 }
-
