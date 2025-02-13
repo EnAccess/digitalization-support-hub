38fce35bf90ee12aa93bd973d8dac465924eea85
mark8-1 <munyaarmel61@gmail.com>
2025-02-13 11:24:07 +0200

    fix header shadow

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export default function Header() {
   return (
-    <header className="bg-white shadow-sm py-4">
+    <header className="bg-white py-4">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center justify-center">
         <Link
           href="/"
