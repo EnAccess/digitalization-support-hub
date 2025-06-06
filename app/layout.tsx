baff90fc2864dce07c25596befe043599c2a6a90
mark8-1 <munyaarmel61@gmail.com>
2025-06-06 09:41:19 +0200

    fix build errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next"
-import { Raleway, Merriweather } from "next/font/google"
+import { Raleway } from "next/font/google"
 import "./globals.css"
 import "@ant-design/v5-patch-for-react-19"
 import { SupportBubbleProvider } from "./components/support-bubble-provider"
@@ -7,13 +7,6 @@ import { SupportBubbleProvider } from "./components/support-bubble-provider"
 const raleway = Raleway({
   subsets: ["latin"],
   variable: "--font-raleway",
-  display: "swap",
-})
-
-const merriweather = Merriweather({
-  weight: ["300", "400", "700", "900"],
-  subsets: ["latin"],
-  variable: "--font-merriweather",
   display: "swap",
 })
 
