e4d25f22e6751e4042156adfa10c53ee21009a64
mark8-1 <munyaarmel61@gmail.com>
2025-07-18 10:37:23 +0200

    fix banner color

--- app/components/suggestion-banner.tsx
+++ app/components/suggestion-banner.tsx
@@ -8,20 +8,20 @@ export function SuggestionBanner() {
   if (!isVisible) return null
 
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-orange-500 text-white p-4 flex justify-between items-center">
+    <div className="fixed bottom-0 left-0 right-0 bg-orange-500 text-[#0D261A] font-medium text-medium p-4 flex justify-between items-center">
       <div className="flex items-center gap-2">
         <span>Know of a relevant tool that&apos;s not listed?</span>
         <Link
           target="_blank"
           href="https://enaccess.typeform.com/new-tool"
-          className="underline hover:text-[#8BDC7F]"
+          className="underline hover:text-[#07140e]"
         >
           Send us your suggestions
         </Link>
       </div>
       <button
         onClick={() => setIsVisible(false)}
-        className="hover:text-[#8BDC7F]"
+        className="hover:text-[#0D261A]"
       >
         <X size={24} />
       </button>
